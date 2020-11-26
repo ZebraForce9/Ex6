d7115a874760f6e16aeeb3c0626336b7820c1f75
ZebraForce9 <58855640+ZebraForce9@users.noreply.github.com>
2020-11-26 21:51:12 +0200

    added 2 get_audio_data() and speed_up_audio()

--- wave_editor.py
+++ wave_editor.py
@@ -2,6 +2,13 @@ from wave_helper import *
 from typing import List
 
 
-def reverse_audio(filename: str):
-    audio_data: List[List[int]] = load_wave(filename)[1]
+def get_audio_data(filename: str) -> List[List[int]]:
+    return load_wave(filename)[1]
+
+
+def reverse_audio(audio_data: List[List[int]]) -> List[List[int]]:
     return audio_data[::-1]
+
+
+def speed_up_audio(audio_data: List[List[int]]) -> List[List[int]]:
+    return audio_data[::2]
