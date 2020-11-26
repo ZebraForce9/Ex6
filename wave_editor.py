ff029a5b6e60812fc38b35ad1236517ef2dcc388
ZebraForce9 <58855640+ZebraForce9@users.noreply.github.com>
2020-11-26 22:58:31 +0200

    removed increase_volume()

--- wave_editor.py
+++ wave_editor.py
@@ -1,6 +1,6 @@
 from wave_helper import *
 from typing import List
-import numpy as np
+
 
 def get_audio_data(filename: str) -> List[List[int]]:
     return load_wave(filename)[1]
@@ -20,11 +20,4 @@ def slow_down_audio(audio_data: List[List[int]]) -> List[List[int]]:
         channel_one = (audio_data[i-1][0] + audio_data[i][0]) // 2
         channel_two = (audio_data[i-1][1] + audio_data[i][1]) // 2
         audio_data.insert(i, [channel_one, channel_two])
-    return audio_data
-
-
-def increase_volume(audio_data: List[List[int]]) -> List[List[int]]:
-    for i, pair in enumerate(audio_data):
-        if pair[0] * 1.2 <= MAX_VOLUME and pair[1] * 1.2 <= MAX_VOLUME:
-            audio_data[i] = pair * 1.2
     return audio_data
