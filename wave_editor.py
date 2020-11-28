0f2a04a6d4e26531a0219075086002304b94d0ab
ZebraForce9 <58855640+ZebraForce9@users.noreply.github.com>
2020-11-28 13:33:20 +0200

    added docstrings and type hints

--- wave_editor.py
+++ wave_editor.py
@@ -19,10 +19,13 @@ MODIFICATION_MESSAGE = ("Selects which modification you would like to preform:\n
                         "8.Exit to Main Menu\n")
 
 
+def main() -> None:
+    """
+    Show the main menu to the user.
 
-def main():
-    """The main function that presents the menu to the user,
-    and shows the different options."""
+    Returns:
+        None
+    """
 
     while True:
         user_input = input("Choose category:\n1.Modify existing wav file\n"
@@ -42,22 +45,37 @@ def main():
             pass
 
 
-def sound_negation(audio_list):
-    """Receives an audio list, changes the sample values to their opposite number"""
-    new_audio_list = []
+def sound_negation(audio_data: List[List[int]]) -> List[List[int]]:
+    """
+    Changes the sample values in the audio data to their opposite number.
 
-    for sample in audio_list:
+    Args:
+        audio_data: A list of audio samples in two channels.
+
+    Returns:
+        The modified list.
+    """
+    new_audio_data: List[List[int]] = []
+
+    for sample in audio_data:
         new_sample = []
         for num in sample:
             new_num = - num
             new_sample.append(adjust_audio_range(new_num))
-        new_audio_list.append(new_sample)
-    return new_audio_list
+        new_audio_data.append(new_sample)
+    return new_audio_data
 
 
-def adjust_audio_range(num):
-    """Checks if a number fits the audio range, returns the number if it is,
-    if not, returns the max/min of the audio range"""
+def adjust_audio_range(num: int) -> int:
+    """
+    Check if a number fits the audio range. Return it if it is, return the max/min of the audio range if not.
+
+    Args:
+        num: A value of a sample we check.
+
+    Returns:
+        The number if it's in the range, max/min value if not.
+    """
 
     if num > MAX_AUDIO:
         return MAX_AUDIO
@@ -67,47 +85,99 @@ def adjust_audio_range(num):
         return num
 
 
-def increase_volume(audio_list):
-    """Increases all the samples by 1.2"""
-    new_sound_list = []
+def increase_volume(audio_data: List[List[int]]) -> List[List[int]]:
+    """
+    Increase all the samples by 1.2.
 
-    for sample in audio_list:
+    Args:
+        audio_data: A list of audio samples in two channels.
+
+    Returns:
+        The modified list.
+    """
+    new_audio_data: List[List[int]] = []
+
+    for sample in audio_data:
         new_sample = []
         for num in sample:
             new_num = num * 1.2
             new_sample.append(adjust_audio_range(int(new_num)))
-        new_sound_list.append(new_sample)
+        new_audio_data.append(new_sample)
 
-    return new_sound_list
+    return new_audio_data
 
 
-def decrease_volume(audio_list):
-    """Increases all the samples by 1.2"""
-    new_sound_list = []
+def decrease_volume(audio_data: List[List[int]]) -> List[List[int]]:
+    """
+    Decrease all the samples by 1.2.
 
-    for sample in audio_list:
+    Args:
+        audio_data: A list of audio samples in two channels.
+
+    Returns:
+        The modified list.
+    """
+    new_audio_data = []
+
+    for sample in audio_data:
         new_sample = []
         for num in sample:
             new_num = num / 1.2
             new_sample.append(adjust_audio_range(int(new_num)))
-        new_sound_list.append(new_sample)
+        new_audio_data.append(new_sample)
 
-    return new_sound_list
+    return new_audio_data
 
 
 def get_audio_data(filename: str) -> List[List[int]]:
+    """
+    Get the audio samples from a wav file.
+
+    Args:
+        filename: The path to the wav file.
+
+    Returns:
+        A list of audio samples in two channels.
+    """
     return load_wave(filename)[1]
 
 
 def reverse_audio(audio_data: List[List[int]]) -> List[List[int]]:
+    """
+    Reverse audio data.
+
+    Args:
+        audio_data: A list of audio samples in two channels.
+
+    Returns:
+        The modified list.
+    """
     return audio_data[::-1]
 
 
 def speed_up_audio(audio_data: List[List[int]]) -> List[List[int]]:
+    """
+    Speed up audio data.
+
+    Args:
+        audio_data: A list of audio samples in two channels.
+
+    Returns:
+        The modified list.
+    """
     return audio_data[::2]
 
 
 def slow_down_audio(audio_data: List[List[int]]) -> List[List[int]]:
+    """
+    Slow down audio data.
+
+    Args:
+        audio_data: A list of audio samples in two channels.
+
+    Returns:
+        The modified list.
+    """
     new_pairs: int = (len(audio_data)-1)*2
     for i in range(1, new_pairs, 2):
         channel_1: int = int((audio_data[i - 1][0] + audio_data[i][0]) / 2)
@@ -117,11 +187,20 @@ def slow_down_audio(audio_data: List[List[int]]) -> List[List[int]]:
 
 
 def low_pass_filter(audio_data: List[List[int]]) -> List[List[int]]:
-    new_audio_data = []
+    """
+    Use a low pass filter on the audio data.
+
+    Args:
+        audio_data: A list of audio samples in two channels.
+
+    Returns:
+        The modified list.
+    """
+    new_audio_data: List[List[int]] = []
     for i, pair in enumerate(audio_data):
         if i == 0:
-            channel_1 = int((pair[0] + audio_data[i + 1][0]) / 2)
-            channel_2 = int((pair[1] + audio_data[i + 1][1]) / 2)
+            channel_1: int = int((pair[0] + audio_data[i + 1][0]) / 2)
+            channel_2: int = int((pair[1] + audio_data[i + 1][1]) / 2)
             new_audio_data.append([channel_1, channel_2])
         elif i == len(audio_data)-1:
             channel_1 = int((pair[0] + audio_data[i - 1][0]) / 2)
